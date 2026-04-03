72cb048510011fc2ff0baaa72e728873173d2e58
Sergey Akishin <morales66@mail.ru>
2026-04-03 16:10:28 +0300

    добавлен компонент explore

--- src/components/Explore/Explore.tsx
+++ src/components/Explore/Explore.tsx
@@ -7,19 +7,33 @@ export const Explore = () => {
 	return (
 		<section className={styles.exploreSection}>
 			<div className={styles.info}>
-				<Tag title="explore"/>
+				<Tag title="explore" />
 				<h2 className={styles.title}>Our diverse Palette</h2>
 				<p className={styles.text}>If you are a breakfast enthusiast, a connoisseur of savory delights, or on the lookout for irresistible desserts, our curated selection has something to satisfy every palate.</p>
-				<Button name="see more"/>
+				<Button name="see more" />
 			</div>
 			<div className={styles.categories}>
-				<IconBreakfest />
-				<IconLunch />
-				<IconDinner />
-				<IconDessert />
-				<IconQuickBite />
+				<div className={styles.category}>
+					<IconBreakfest />
+					<p>breakfest</p>
+				</div>
+				<div className={styles.category}>
+					<IconLunch />
+					<p>lunch</p>
+				</div>
+				<div className={styles.category}>
+					<IconDinner />
+					<p>dinner</p>
+				</div>
+				<div className={styles.category}>
+					<IconDessert />
+					<p>dessert</p>
+				</div>
+				<div className={styles.category}>
+					<IconQuickBite />
+					<p>quick bite!</p>
+				</div>
 			</div>
-
 		</section>
 	)
 }
